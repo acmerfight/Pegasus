8e62c9847d69899bd5206cf5ed536449deb087ec
acmerfight <acmerfight@gmail.com>
2015-02-06 23:48:09 +0800

    ad sc_sup

---
+++ ebin/sc_sup.erl
@@ -0,0 +1,21 @@
+-module(sc_sup).
+
+-behaviour(supervisor).
+
+-export(
+    [
+        start_link/0,
+        start_child/2
+    ]
+).
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+init([]) ->
+    Element = {sc_element, {sc_element, start_link, []},
+               temporary, brutal_kill, worker, [sc_element]},
+    Children = [Element],
+    RestartStrategy = {simple_one_for_one, 0, 1},
+    {ok, {RestartStrategy, Children}}.
