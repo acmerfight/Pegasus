421791386ac2932d204c9ad009058813959d0c9a
acmerfight <acmerfight@gmail.com>
2015-02-09 23:06:37 +0800

    add interface

--- src/sc_element.erl
+++ src/sc_element.erl
@@ -17,3 +17,42 @@
 -define(DEFAULT_LEASE_TIME, (60 * 60 * 24)).
 
 -record(state, {value, lease_time, start_time}).
+
+start_link(Value, LeaseTime) ->
+    sc_sup:start_link(?MODULE, [Value, lease_time], []).
+
+create(Value, LeaseTime) ->
+    sc_sup:start_child(Value, LeaseTime).
+
+create(Value) ->
+    create(Value, ?DEFAULT_LEASE_TIME).
+
+fetch(Pid) ->
+    gen_server:call(Pid, fetch).
+
+replace(Pid, Value) ->
+    gen_server:cast(Pid, {replace, Value}).
+
+delete(Pid) ->
+    gen_server:cast(Pid, delete).
+
+
+init([Value, LeaseTime]) ->
+    Now = calender:local_time(),
+    StartTime = calender:datetime_to_gregorian_seconds(Now),
+    {ok,
+        #state{value = Value,
+              lease_time = LeaseTime,
+              start_time = StartTime},
+        time_left(StartTime, LeaseTime)}.
+
+time_left(_StartTime, infinity) ->
+    infinity;
+time_left(StartTime, LeaseTime) ->
+    Now = calender:local_time(),
+    CurrentTime = calender:datetime_to_gregorian_seconds(Now),
+    TimeElapsed = CurrentTime - StartTime,
+    case LeaseTime - TimeElapsed of
+        Time when Time =< 0 -> 0;
+        Time -> Time * 1000
+    end.
