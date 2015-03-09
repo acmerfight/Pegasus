0537c917b265feebedc1b69b73db16821c3efcc9
acmerfight <acmerfight@gmail.com>
2015-03-09 22:37:37 +0800

    add minesa

--- src/sc_app.erl
+++ src/sc_app.erl
@@ -5,6 +5,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    ok = ensure_contact(),
     % 存储初始化
     sc_store:init(),
     % 启动根监督者
@@ -15,6 +16,49 @@ start(_StartType, _StartArgs) ->
         Other ->
             {error, Other}
     end.
+
+ensure_contact() ->
+    DefaultNodes = ["a@localhost", "b@localhost"],
+    case get_env(simple_cache, contact_nodes, DefaultNodes) of
+        [] ->
+            {error, no_contact_nodes};
+        Contact_nodes ->
+            ensure_contact(Contact_nodes)
+    end.
+
+get_env(AppName, Key, Default) ->
+    case application:get_env(AppName, Key) of
+        undefined -> Default;
+        {ok, Value} -> Value
+    end.
+
+ensure_contact(ContactNodes) ->
+    Answering = [N || N <- ContactNodes, net_adm:ping(N) =:= pong],
+    case Answering of
+        [] ->
+            {error, no_contact_nodes_reachable};
+        _ ->
+            DefaultTime = 6000,
+            WaitTime = get_env(simple_cache, wait_time, DefaultTime),
+            wait_for_nodes(length(Answering), WaitTime)
+        end.
+
+wait_for_nodes(MinNodes, WaitTime) ->
+    Slices = 10,
+    SliceTime = round(WaitTime/Slices),
+    wait_for_nodes(MinNodes, SliceTime, Slices).
+
+wait_for_nodes(_MinNodes, _SliceTime, 0) ->
+    ok;
+wait_for_nodes(MinNodes, SliceTime, Iterations) ->
+    case length(nodes()) > MinNodes of
+        true ->
+            ok;
+        false ->
+            timer:sleep(SliceTime),
+            wait_for_nodes(MinNodes, SliceTime, Iterations - 1)
+    end.
+
 
 stop(_State) ->
     ok.
--- src/sc_store.erl
+++ src/sc_store.erl
@@ -9,18 +9,27 @@
 
 -define(TABLE_ID, ?MODULE).
 
+-record(key_to_pid, {key, pid}).
+
 init() ->
-    ets:new(?TABLE_ID, [public, named_table]),
-    ok.
+    mnesia:start(),
+    mnesia:create_table(key_to_pid,
+                        [{index, [pid]},
+                         {attributes, record_info(fields, key_to_pid)}]).
 
 insert(Key, Pid) ->
-    ets:insert(?TABLE_ID, {Key, Pid}).
+    mnesia:dirty_write(#key_to_pid{key = Key, pid = Pid}).
 
 lookup(Key) ->
-    case ets:lookup(?TABLE_ID, Key) of
-        [{Key, Pid}] -> {ok, Pid};
+    case mnesia:dirty_read(key_to_pid, Key) of
+        [{key_to_pid, Key, Pid}] ->{ok, Pid};
         [] -> {error, not_found}
     end.
 
 delete(Pid) ->
-    ets:match_delete(?TABLE_ID, {'_', Pid}).
+    case mnesia:dirty_index_read(key_to_pid, Pid, #key_to_pid.pid) of
+        [#key_to_pid{} = Record] ->
+            mnesia:dirty_delete_object(Record);
+        _ ->
+            ok
+    end.
