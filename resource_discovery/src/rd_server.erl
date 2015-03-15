ef66a0bf0b66318b98f5e5f91ead8d5b5620295e
acmerfight <acmerfight@gmail.com>
2015-03-15 10:52:36 +0800

    complete rd_server.erl

--- resource_discovery/src/rd_server.erl
+++ resource_discovery/src/rd_server.erl
@@ -10,7 +10,7 @@
     trade_resources/0
     ]).
 
--export([init/1, handle_call/3, handle_cast/3, handle_info/2,
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
         terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
@@ -50,10 +50,44 @@ handle_cast({add_local_resource, {Type, Resource}}, State) ->
     ResourceTuples = State#state.local_resource_tuples,
     NewResourceTuples = add_resource(Type, Resource, ResourceTuples),
     {noreply, State#state{local_resource_tuples = NewResourceTuples}};
+handle_cast(trade_resources, State) ->
+    ResourceTuples = State#state.local_resource_tuples,
+    AllNodes = [node() | nodes()],
+    lists:foreach(
+        fun(Node) ->
+                gen_server:cast({?SERVER, Node},
+                                {trade_resources, {node(), ResourceTuples}})
+        end,
+        AllNodes),
+    {noreply, State};
+handle_cast({trade_resources, {ReplyTo, Remotes}},
+            #state{local_resource_tuples = Locals,
+                  target_resource_types = TargetTypes,
+                  found_resource_tuples = OldFound} = State) ->
+    FilteredRemotes = resources_for_types(TargetTypes, Remotes),
+    NewFound = add_resources(FilteredRemotes, OldFound),
+    case ReplyTo of
+        noreply ->
+            ok;
+        _ ->
+            gen_server:cast({?SERVER, ReplyTo},
+                            {trade_resources, {noreply, Locals}})
+    end,
+    {noreply, State#state{found_resource_tuples = NewFound}}.
 
 handle_info(ok = _Info, State) ->
     {noreply, State}.
 
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+add_resources([{Type, Identifier}|T], Dict) ->
+    add_resources(T, add_resource(Type, Identifier, Dict));
+add_resources([], Dict) ->
+    Dict.
 
 add_resource(Type, Resource, Dict) ->
     case dict:find(Type, Dict) of
@@ -63,3 +97,15 @@ add_resource(Type, Resource, Dict) ->
         error ->
             dict:store(Type, [Resource], Dict)
     end.
+
+resources_for_types(Types, ResourceTuples) ->
+    Fun =
+          fun(Type, Acc) ->
+            case dict:find(Type, ResourceTuples) of
+                {ok, List} ->
+                    [{Type, Resource} || Resource <- List] ++ Acc;
+                error ->
+                    Acc
+            end
+        end,
+    lists:foldl(Fun, [], Types).
